fadea5e6e74b5a0736a47f10683c94a3ab9dd4ee
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-02-20 17:54:27 +0800

    notification模块 语言包升级

--- configs/jslang.php
+++ configs/jslang.php
@@ -44,43 +44,20 @@
 //
 //  ---------------------------------------------------------------------------------
 //
-defined('IN_ECJIA') or exit('No permission resources.');
 
 /**
- * 消息中心标记为已读
- * @author will.chen
+ * js语言包设置
  */
-class admin_merchant_notification_read_module extends api_admin implements api_interface {
-	
-    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    	
-    	if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
-            return new ecjia_error(100, 'Invalid session');
-        }
-		
-    	$message_id = $this->requestData('id');
-    	if (empty($message_id)) {
-    		return new ecjia_error('invalid_parameter', __('参数无效', 'notification'));
-    	}
-    	
-    	$db = RC_DB::table('notifications');
-    	if ($_SESSION['staff_id']) {
-    		$db->where('notifiable_type', 'orm_staff_user_model')->where('notifiable_id', $_SESSION['staff_id']);
-    	} elseif ($_SESSION['admin_id']) {
-    	    
-    	}
-    	$notification_info = $db->where('id', $message_id)->first();
-    	if (empty($notification_info)) {
-    		return new ecjia_error('notification_not_exists', '该消息不存在！');
-    	}
-    	if (!empty($notification_info['read_at'])) {
-    		return new ecjia_error('notification_already_read', '该消息已读！');
-    	} 
-    	
-    	RC_DB::table('notifications')->where('id', $message_id)->update(array('read_at' => RC_Time::local_date('Y-m-d H:i:s', RC_Time::gmtime())));
-    	
-		return array();
-	 }	
-}
 
-// end
+defined('IN_ECJIA') or exit('No permission resources.');
+
+return array(
+    //notification
+    'notification_page' =>array(
+        'ok' =>  __('确定', 'notification'),
+        'cancel' =>  __('取消', 'notification'),
+
+    ),
+
+);
+//end
